b4a9be1765d4f537381d963e7ba76bc0f3f39816
boscotsang <boscotsang3@gmail.com>
2016-05-04 14:33:16 +0800

    no bn & add flexible layers

--- source/layers/layer.py
+++ source/layers/layer.py
@@ -1,8 +1,6 @@
-
 class Layer(object):
-
     def __call__(self, input):
         return self.forward(input)
 
-    def forward(self,input):
+    def forward(self, input):
         raise NotImplementedError()
